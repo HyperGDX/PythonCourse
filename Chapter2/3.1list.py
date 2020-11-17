67096d5d5c9b9181ea3673c36aafa70d4c5fe1e6
HyperGDX <1050264043@qq.com>
2020-11-17 10:18:49 +0800

    11.17

---
+++ Chapter2/3.1list.py
@@ -0,0 +1,14 @@
+message = "this is a string example. wow!"
+print(message.rsplit())
+#['this', 'is', 'a', 'string', 'example.', 'wow!']
+#python中用 [] 表示列表
+
+#访问列表元素
+list_1 = message.rsplit()
+print(list_1[0])  #输出元素不带方括号
+print(list_1[0].upper())
+print(list_1[-1])
+
+message_1 = f"A {list_1[3].upper()} example"
+print(message_1)
+
